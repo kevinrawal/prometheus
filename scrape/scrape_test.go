ad29b1c2565378c4fff19605e20a117f73793053
Arthur Silva Sens <arthursens2005@gmail.com>
2024-07-19 11:28:00 -0300

    WIP: Histogram CT Zero ingestion
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- scrape/scrape_test.go
+++ scrape/scrape_test.go
@@ -1994,7 +1994,8 @@ metric: <
 `,
 			contentType: "application/vnd.google.protobuf",
 			histograms: []histogramSample{{
-				t: 1234568,
+				t:      1234568,
+				metric: labels.FromStrings("__name__", "test_histogram"),
 				h: &histogram.Histogram{
 					Count:         175,
 					ZeroCount:     2,
@@ -2120,7 +2121,8 @@ metric: <
 				{metric: labels.FromStrings("__name__", "test_histogram_bucket", "le", "+Inf"), t: 1234568, f: 175},
 			},
 			histograms: []histogramSample{{
-				t: 1234568,
+				t:      1234568,
+				metric: labels.FromStrings("__name__", "test_histogram"),
 				h: &histogram.Histogram{
 					Count:         175,
 					ZeroCount:     2,
